9f326588693035083e48494eca88ebd7f116dc64
Daniel Sanchez <danicanod@gmail.com>
2023-04-29 22:28:04 -0400

    seventh commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // index.js
 const express = require('express')
+const { google } = require('googleapis')
+const { reseller } = require('googleapis/build/src/apis/reseller')
 
 const app = express()
 const PORT = 4000
@@ -10,8 +12,26 @@ app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.send('Hey this is my API running 🚀🚀')
+  
+  const auth = new google.auth.GoogleAuth({
+    keyFile: './files/credentials.json',
+    scopes: 'https://www.googleapis.com/auth/spreadsheets'
+  })
+
+  const client = await auth.getClient()
+
+  const googleSheets = google.sheets({version: 'v4', auth: client})
+
+  const spreadsheetId = '1KL10-5shnLdg53A9lizK49N95xFK0Xamye4O3GCMouw'
+
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  })
+
+  res.send(metaData)
 })
 
 app.post('/webhook', (req, res) => {
