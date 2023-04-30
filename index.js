a24049bffa399c079d005e2b4c1fe55bd1fd166b
Daniel Sanchez <danicanod@gmail.com>
2023-04-29 22:57:14 -0400

    seventh commit

--- index.js
+++ index.js
@@ -1,10 +1,73 @@
 // index.js
 const express = require('express')
-const { google } = require('googleapis')
-const { reseller } = require('googleapis/build/src/apis/reseller')
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
+
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
 const app = express()
 const PORT = 4000
+
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+async function listMajors(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: '1KL10-5shnLdg53A9lizK49N95xFK0Xamye4O3GCMouw',
+    range: 'Productos!A1:F1',
+  });
+  const rows = res.data.values;
+  if (!rows || rows.length === 0) {
+    console.log('No data found.');
+    return;
+  }
+  console.log('Name, Major:');
+  rows.forEach((row) => {
+    // Print columns A and E, which correspond to indices 0 and 4.
+    console.log(`${row[0]}, ${row[4]}`);
+  });
+}
 
 app.use(express.json())
 
@@ -14,24 +77,8 @@ app.listen(PORT, () => {
 
 app.get('/', async (req, res) => {
   // res.send('Hey this is my API running 🚀🚀')
-  
-  const auth = new google.auth.GoogleAuth({
-    keyFile: 'credentials.json',
-    scopes: 'https://www.googleapis.com/auth/spreadsheets'
-  })
 
-  const client = await auth.getClient()
-
-  const googleSheets = google.sheets({version: 'v4', auth: client})
-
-  const spreadsheetId = '1KL10-5shnLdg53A9lizK49N95xFK0Xamye4O3GCMouw'
-
-  const metaData = await googleSheets.spreadsheets.get({
-    auth,
-    spreadsheetId,
-  })
-
-  res.send(metaData)
+  res.send(authorize().then(listMajors).catch(console.error);)
 })
 
 app.post('/webhook', (req, res) => {
